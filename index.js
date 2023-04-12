57a3bfb140dcbf2ca560e9d4ea521901a6f9f4f8
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-04-12 14:19:50 +0900

    2023.04.12_14:19

--- index.js
+++ index.js
@@ -12,27 +12,12 @@ var section02 = document.querySelector('.section02');
 var cursor = document.querySelector('.cursor');
 window.addEventListener('scroll', function(){
     var window_sy = window.scrollY;
-    //console.log(window_sy);
     // 각 섹션마다 ani 클래스 추가
     for(var i=0; i<section.length; i++){
         if(window_sy >= section[i].offsetTop){
             section[i].classList.add('ani');
         }
     }
-
-    // 스크롤 값이 섹션02 works에 위치할 때 body에 클래스 추가 
-    if(window.scrollY >= section02.offsetTop){
-        document.body.classList.add('cursor_active');
-    }else{
-        document.body.classList.remove('cursor_active');
-    }
-    // body에 cursor_active 클래스 존재 시 cursor 태그에 클래스 추가
-    if(document.body.classList.contains('cursor_active') === true){
-        cursor.classList.add('active');
-    }else{
-        cursor.classList.remove('active');
-    }
-    
 });
 // 섹션02 위치할 때 커스텀한 커서 모양 노출되도록 
 document.addEventListener('DOMContentLoaded', function(){
@@ -40,7 +25,20 @@ document.addEventListener('DOMContentLoaded', function(){
         cursor.style.left = `${e.clientX}px`;
         cursor.style.top = `${e.clientY}px`;
     }
+    
 });
+// 스크롤 값이 섹션02 works에 위치할 때 body에 클래스 추가 
+if(window.pageYOffset >= section02.getBoundingClientRect().top){
+    document.body.classList.add('cursor_active');
+}else{
+    document.body.classList.remove('cursor_active');
+}
+// body에 cursor_active 클래스 존재 시 cursor 태그에 클래스 추가
+if(document.body.classList.contains('cursor_active') === true){
+    cursor.classList.add('active');
+}else{
+    cursor.classList.remove('active');
+}
 // 섹션02 각각의 li 호버 시 스크롤 값 변경되도록
 var section02_li = document.querySelectorAll('.section02-inner ul li');
 for(var i=0; i<section02_li.length; i++){
@@ -57,7 +55,6 @@ for(var i=0; i<section02_li.length; i++){
         })
         */
         var this_li_top = this_li.getBoundingClientRect().top + window.pageYOffset / 1.05;
-        console.log(this_li_top);
         window.scroll({top:this_li_top, behavior:'smooth'});
     });
 }
