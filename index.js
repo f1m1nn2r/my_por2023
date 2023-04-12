940cfbc7c1495fb7f1825e8070fbbb9f9a179b3d
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-04-12 13:54:17 +0900

    2023.04.12_13:54

--- index.js
+++ index.js
@@ -8,12 +8,52 @@ window.onload = function(){
 }
 // 윈도우 스크롤 이벤트
 var section = document.querySelectorAll('.section');
+var section02 = document.querySelector('.section02');
+var cursor = document.querySelector('.cursor');
 window.addEventListener('scroll', function(){
     var window_sy = window.scrollY;
+    console.log(window_sy);
     // 각 섹션마다 ani 클래스 추가
     for(var i=0; i<section.length; i++){
         if(window_sy >= section[i].offsetTop){
             section[i].classList.add('ani');
         }
     }
+
+    // 스크롤 값이 섹션02 works에 위치할 때 body에 클래스 추가 
+    if(window.scrollY >= section02.offsetTop){
+        document.body.classList.add('cursor_active');
+    }else{
+        document.body.classList.remove('cursor_active');
+    }
+    // body에 cursor_active 클래스 존재 시 cursor 태그에 클래스 추가
+    if(document.body.classList.contains('cursor_active') === true){
+        cursor.classList.add('active');
+    }else{
+        cursor.classList.remove('active');
+    }
+    
 });
+// 섹션02 위치할 때 커스텀한 커서 모양 노출되도록 
+document.addEventListener('DOMContentLoaded', function(){
+    document.body.onmousemove = function(e){
+        cursor.style.left = `${e.clientX}px`;
+        cursor.style.top = `${e.clientY}px`;
+    }
+});
+// 섹션02 각각의 li 호버 시 스크롤 값 변경되도록
+var section02_li = document.querySelectorAll('.section02-inner ul li');
+for(var i=0; i<section02_li.length; i++){
+    section02_li[i].addEventListener('mouseover', function(e){
+        //var this_li = this;
+        //var this_li_top = this_li.offsetTop;
+        var percent = e.clientY / window.innerHeight;
+        var section02_inner = document.querySelector('.section02-inner');
+        section02_inner.animate({
+            transform: `translateY(${percent * section02_inner.offsetHeight * -1}px)`
+        }, {
+            fill:"forwards",
+            duration:4000,
+        })
+    });
+}
