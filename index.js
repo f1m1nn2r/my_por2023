f7b93b2c959a96a5b2be2933f486777a1dd50fa4
김민중 <93234748+fe2oz@users.noreply.github.com>
2023-04-12 14:26:46 +0900

    2023.04.12_14:26

--- index.js
+++ index.js
@@ -43,18 +43,19 @@ if(document.body.classList.contains('cursor_active') === true){
 var section02_li = document.querySelectorAll('.section02-inner ul li');
 for(var i=0; i<section02_li.length; i++){
     section02_li[i].addEventListener('mouseover', function(e){
-        var this_li = this;
-        /*
+        //var this_li = this;
+
         var percent = e.clientY / window.innerHeight;
         var section02_inner = document.querySelector('.section02-inner');
         section02_inner.animate({
-            transform: `translateY(${percent * section02_inner.offsetHeight * -1}px)`
+            transform:`translateY(${percent * section02_inner.offsetHeight * -0.5}px)`
         }, {
             fill:"forwards",
             duration:4000,
         })
-        */
-        var this_li_top = this_li.getBoundingClientRect().top + window.pageYOffset / 1.05;
+        /*
+        var this_li_top = this_li.getBoundingClientRect().top + window.pageYOffset;
         window.scroll({top:this_li_top, behavior:'smooth'});
+        */
     });
 }
